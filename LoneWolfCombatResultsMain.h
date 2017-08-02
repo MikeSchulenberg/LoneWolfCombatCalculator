6b5a2fa6ed2d2fa8adadb67c8d3d74c8bdc39019
Mike Schulenberg <mike.schulenberg@gmail.com>
2017-08-02 12:11:52 -0500

    First attempt at a Combat Skill panel for the GUI.
    Appearance is fine so far. The behavior when resizing the window isn't quite right. I intend to experiment with adding more wxPanels.

--- LoneWolfCombatResultsMain.h
+++ LoneWolfCombatResultsMain.h
@@ -11,14 +11,10 @@
 #define LONEWOLFCOMBATRESULTSMAIN_H
 
 //(*Headers(LoneWolfCombatResultsFrame)
-#include <wx/notebook.h>
 #include <wx/sizer.h>
 #include <wx/stattext.h>
 #include <wx/menu.h>
-#include <wx/textctrl.h>
 #include <wx/panel.h>
-#include <wx/gbsizer.h>
-#include <wx/button.h>
 #include <wx/frame.h>
 #include <wx/combobox.h>
 #include <wx/statusbr.h>
@@ -47,15 +43,6 @@ class LoneWolfCombatResultsFrame: public wxFrame
         static const long ID_COMBOBOX2;
         static const long ID_STATICTEXT3;
         static const long ID_STATICTEXT4;
-        static const long ID_BUTTON2;
-        static const long ID_PANEL4;
-        static const long ID_PANEL2;
-        static const long ID_STATICTEXT5;
-        static const long ID_TEXTCTRL1;
-        static const long ID_BUTTON1;
-        static const long ID_PANEL5;
-        static const long ID_PANEL3;
-        static const long ID_NOTEBOOK1;
         static const long ID_PANEL1;
         static const long idMenuQuit;
         static const long idMenuAbout;
@@ -63,21 +50,12 @@ class LoneWolfCombatResultsFrame: public wxFrame
         //*)
 
         //(*Declarations(LoneWolfCombatResultsFrame)
-        wxPanel* Panel5;
-        wxNotebook* Notebook1;
         wxStaticText* StaticText2;
-        wxPanel* Panel4;
-        wxButton* Button1;
         wxPanel* Panel1;
         wxStaticText* StaticText1;
         wxStaticText* StaticText3;
-        wxButton* Button2;
-        wxPanel* Panel3;
-        wxStaticText* StaticText5;
         wxStatusBar* StatusBar1;
         wxComboBox* ComboBox1;
-        wxTextCtrl* TextCtrl1;
-        wxPanel* Panel2;
         wxComboBox* ComboBox2;
         wxStaticText* StaticText4;
         //*)
