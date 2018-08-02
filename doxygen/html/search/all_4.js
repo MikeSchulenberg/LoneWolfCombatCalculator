909ea706b25b60132e4fa56c56de2b9af40ba52c
Mike Schulenberg <31629046+MikeSchulenberg@users.noreply.github.com>
2018-08-02 14:39:00 -0500

    Delete all_4.js

--- doxygen/html/search/all_4.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['lonewolfcombatresultsapp',['LoneWolfCombatResultsApp',['../class_lone_wolf_combat_results_app.html',1,'']]],
-  ['lonewolfcombatresultsapp_2ecpp',['LoneWolfCombatResultsApp.cpp',['../_lone_wolf_combat_results_app_8cpp.html',1,'']]],
-  ['lonewolfcombatresultsapp_2eh',['LoneWolfCombatResultsApp.h',['../_lone_wolf_combat_results_app_8h.html',1,'']]],
-  ['lonewolfcombatresultsframe',['LoneWolfCombatResultsFrame',['../class_lone_wolf_combat_results_frame.html',1,'']]],
-  ['lonewolfcombatresultsmain_2ecpp',['LoneWolfCombatResultsMain.cpp',['../_lone_wolf_combat_results_main_8cpp.html',1,'']]],
-  ['lonewolfcombatresultsmain_2eh',['LoneWolfCombatResultsMain.h',['../_lone_wolf_combat_results_main_8h.html',1,'']]]
-];
