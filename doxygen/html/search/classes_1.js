daff8d08630bd67c04bd046008d178f42f1ad3a6
Mike Schulenberg <31629046+MikeSchulenberg@users.noreply.github.com>
2018-08-02 14:41:06 -0500

    Delete classes_1.js

--- doxygen/html/search/classes_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['lonewolfcombatresultsapp',['LoneWolfCombatResultsApp',['../class_lone_wolf_combat_results_app.html',1,'']]],
-  ['lonewolfcombatresultsframe',['LoneWolfCombatResultsFrame',['../class_lone_wolf_combat_results_frame.html',1,'']]]
-];
