5322de15bb5bc567982521bec9f7c6c9d3e4bda7
Mike Schulenberg <31629046+MikeSchulenberg@users.noreply.github.com>
2018-08-02 14:42:19 -0500

    Delete files_2.js

--- doxygen/html/search/files_2.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['lonewolfcombatresultsapp_2ecpp',['LoneWolfCombatResultsApp.cpp',['../_lone_wolf_combat_results_app_8cpp.html',1,'']]],
-  ['lonewolfcombatresultsapp_2eh',['LoneWolfCombatResultsApp.h',['../_lone_wolf_combat_results_app_8h.html',1,'']]],
-  ['lonewolfcombatresultsmain_2ecpp',['LoneWolfCombatResultsMain.cpp',['../_lone_wolf_combat_results_main_8cpp.html',1,'']]],
-  ['lonewolfcombatresultsmain_2eh',['LoneWolfCombatResultsMain.h',['../_lone_wolf_combat_results_main_8h.html',1,'']]]
-];
