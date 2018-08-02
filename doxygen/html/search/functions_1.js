f02baab04708aeb791e35484000ad3e6708f68a1
Mike Schulenberg <31629046+MikeSchulenberg@users.noreply.github.com>
2018-08-02 14:42:48 -0500

    Delete functions_1.js

--- doxygen/html/search/functions_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['invalidateenemycombatskill',['invalidateEnemyCombatSkill',['../class_combat_results.html#a2f29fcdfc687b25aa44616308a3fbd77',1,'CombatResults']]],
-  ['invalidateherocombatskill',['invalidateHeroCombatSkill',['../class_combat_results.html#a866f2995a23c26d5f9ff09f5acb7238f',1,'CombatResults']]]
-];
