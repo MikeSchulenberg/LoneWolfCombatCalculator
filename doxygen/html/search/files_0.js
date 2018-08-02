8026c9d575919d58844dbe86a0c7d5d9b1a0a32e
Mike Schulenberg <31629046+MikeSchulenberg@users.noreply.github.com>
2018-08-02 14:41:51 -0500

    Delete files_0.js

--- doxygen/html/search/files_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['combatresults_2ecpp',['CombatResults.cpp',['../_combat_results_8cpp.html',1,'']]],
-  ['combatresults_2eh',['CombatResults.h',['../_combat_results_8h.html',1,'']]]
-];
