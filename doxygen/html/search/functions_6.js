7214928389e551d0cd7d3be7a36d4a895e470e31
Mike Schulenberg <31629046+MikeSchulenberg@users.noreply.github.com>
2018-08-02 14:43:58 -0500

    Delete functions_6.js

--- doxygen/html/search/functions_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['togglerandomnumbersection',['toggleRandomNumberSection',['../class_lone_wolf_combat_results_frame.html#a51e4683696acb4427474e4706470cc27',1,'LoneWolfCombatResultsFrame']]]
-];
