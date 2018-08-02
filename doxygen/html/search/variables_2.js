59705c93d723be34df03e6430e752228117a4b98
Mike Schulenberg <31629046+MikeSchulenberg@users.noreply.github.com>
2018-08-02 14:45:40 -0500

    Delete variables_2.js

--- doxygen/html/search/variables_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['num_5fcols',['NUM_COLS',['../_combat_results_8cpp.html#ab63d879fa9c2c0a74acdb8cf45a3a9c5',1,'CombatResults.cpp']]],
-  ['num_5frows',['NUM_ROWS',['../_combat_results_8cpp.html#ad9a5ccb2cc434942aaa36a40096906d4',1,'CombatResults.cpp']]]
-];
