f5a93d7e9693af69480b272335b07cb21769011c
Mike Schulenberg <31629046+MikeSchulenberg@users.noreply.github.com>
2018-08-02 14:48:12 -0500

    Delete annotated_dup.js

--- doxygen/html/annotated_dup.js
+++
@@ -1,6 +0,0 @@
-var annotated_dup =
-[
-    [ "CombatResults", "class_combat_results.html", "class_combat_results" ],
-    [ "LoneWolfCombatResultsApp", "class_lone_wolf_combat_results_app.html", "class_lone_wolf_combat_results_app" ],
-    [ "LoneWolfCombatResultsFrame", "class_lone_wolf_combat_results_frame.html", "class_lone_wolf_combat_results_frame" ]
-];
