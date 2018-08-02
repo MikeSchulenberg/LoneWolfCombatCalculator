e8ef2ac198e348984971441b3083e01fe55d4e1d
Mike Schulenberg <31629046+MikeSchulenberg@users.noreply.github.com>
2018-08-02 14:43:38 -0500

    Delete functions_5.js

--- doxygen/html/search/functions_5.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['setdieroll',['setDieRoll',['../class_combat_results.html#a4297eae70c40a9904faa57e6e29dbc87',1,'CombatResults']]],
-  ['setenemycombatskill',['setEnemyCombatSkill',['../class_combat_results.html#a643c875377710a504480dee99a08ec27',1,'CombatResults']]],
-  ['setherocombatskill',['setHeroCombatSkill',['../class_combat_results.html#a7e52e88318db29ca7f6dcc20a697ee3b',1,'CombatResults']]]
-];
