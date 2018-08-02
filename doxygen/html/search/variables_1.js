6b163092eb7d11f121c78a5932597e3c878fd8df
Mike Schulenberg <31629046+MikeSchulenberg@users.noreply.github.com>
2018-08-02 14:45:27 -0500

    Delete variables_1.js

--- doxygen/html/search/variables_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['damage_5fto_5fenemy',['DAMAGE_TO_ENEMY',['../_combat_results_8cpp.html#a26a24b56b7e8709c493aa61d0f7b1834',1,'CombatResults.cpp']]],
-  ['damage_5fto_5fhero',['DAMAGE_TO_HERO',['../_combat_results_8cpp.html#a1cf304058f047994c695446bf3bbfc0d',1,'CombatResults.cpp']]]
-];
